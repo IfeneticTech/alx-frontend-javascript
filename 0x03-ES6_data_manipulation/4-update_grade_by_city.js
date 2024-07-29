a7e4767262b952e0a082bb1115b4f73d4ab87019
IfeneticTech <inehpaul@gmail.com>
2024-07-29 17:21:06 +0000

    Tasks for the 0x03-ES6_data_manipulation project

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,1 @@
-export default function updateStudentGradeByCity(arr, city, newGrades) {
-  return arr
-    .filter(student => student.location === city)
-    .map(student => {
-      let grade = newGrades.find(g => g.studentId === student.id);
-      return { ...student, grade: (grade && grade.grade) || 'N/A' };
-    });
-}
-
+export default function updateStudentGradeByCity(studentList, city, newGrades 
